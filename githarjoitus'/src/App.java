8d792f9201c118a8d75dd0efb6e97d90cc5af88b
Samu Keskitalo <samukeskitalo@hotmail.com>
2025-12-03 16:38:13 +0200

    Samun tulostus

--- githarjoitus'/src/App.java
+++ githarjoitus'/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Tämä on Ronin lisäämä teksti!");
+        System.out.println("Tämä on Samun lisäämä teksti");
     }
 }
